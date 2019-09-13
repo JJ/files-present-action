c78581e1256ececd69f156587b235b7a0747e533
JJ Merelo <jjmerelo@gmail.com>
2019-09-13 06:52:50 +0200

    Add real action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import * as core from '@actions/core';
+import fs from 'fs';
 
 async function run() {
   try {
-      const files = core.getInput('files');
-      console.log( files )
+      const files = core.getInput('files')
+      const fileList = files.split(/,\s+/)
+      console.log(fileList)
+      var doesntExist = ''
+      fileList.forEach( function( file ) {          
+          fs.stat(file, (exists) => {
+              if (exists == null) {
+                  return true;
+              } else if (exists.code === 'ENOENT') {
+                  doesntExist += ", " + file 
+              }
+          });
+          if (doesntExist ) {
+              core.setFailed( "These files do not exist: ${doestnExist}" )
+          }
+      })
+
   } catch (error) {
       core.setFailed(error.message);
   }
