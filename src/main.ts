ac0f357004e2312d14393fd91fc613749f30e2f7
JJ Merelo <jjmerelo@gmail.com>
2019-09-13 06:36:01 +0200

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+      const files = core.getInput('files');
+      console.log( files )
   } catch (error) {
-    core.setFailed(error.message);
+      core.setFailed(error.message);
   }
 }
 
